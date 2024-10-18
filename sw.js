9cdbc89c949373a8b22a79f910b703bfcde0ca6e
Artyom <94465686+o6re4een@users.noreply.github.com>
2024-10-18 19:21:08 +0500

    fix missing sw

---
+++ sw.js
@@ -0,0 +1,43 @@
+const staticCacheName = 'site-static-v1';
+
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/css/style.css',
+    '/images/icons/icon-144x144.png',
+    '/images/icons/icon-192x192.png',
+    '/images/icons/icon-72x72.png',
+    '/images/icons/icon-96x96.png',
+    '/images/icons/icon-128x128.png'
+];  
+
+
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
+})
+
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    )
+})
