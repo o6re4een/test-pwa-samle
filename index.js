4003bc2fa8fc7d10e867311eb66d272ee2516de9
Artyom <94465686+o6re4een@users.noreply.github.com>
2024-10-18 18:59:51 +0500

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+const calculaateBtn = document.getElementById('calculate');
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const result = document.getElementById('result');
+
+calculaateBtn.addEventListener('click', () => {
+    const heightValue = parseFloat(height.value)/100;
+    const weightValue = parseFloat(weight.value);
+    const bmi = weightValue / (heightValue * heightValue);
+    result.innerText = `Your BMI is ${bmi.toFixed(2)}`;    
+});
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(() => console.log('Service Worker registered successfully.'))
+      .catch((error) => console.log('Service Worker registration failed:', error));
+  }
+
+})
+
